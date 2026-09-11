6780c8deeb56472dcd787f7123df138a0d7c7f53
nrupala <nrupalakolkar@gmail.com>
2026-09-11 11:30:20 -0600

    chore: remove scratch

--- deploy/provision/dbg_fc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-grep -iE 'flask-caching|flask_caching' /opt/aecp/venv/lib/python3.12/site-packages/apache_superset-*.dist-info/METADATA | head -3
-/opt/aecp/venv/bin/pip list 2>/dev/null | grep -iE 'flask|superset|pyarrow' | head -12
