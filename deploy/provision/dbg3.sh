71781300d1f6dbe9748f8864b25fb444464d11d8
nrupala <nrupalakolkar@gmail.com>
2026-09-11 11:15:42 -0600

    chore: remove debug scratch scripts from provision dir

--- deploy/provision/dbg3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -n '260,290p' /opt/aecp/venv/lib/python3.12/site-packages/superset/utils/cache_manager.py
-echo '=== metastore factory tail ==='
-sed -n '80,100p' /opt/aecp/venv/lib/python3.12/site-packages/superset/extensions/metastore_cache.py
--- deploy/provision/dbg_cache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sed -n '200,235p' /opt/aecp/venv/lib/python3.12/site-packages/superset/utils/cache_manager.py
-echo '=== metastore_cache.py ==='
-sed -n '40,80p' /opt/aecp/venv/lib/python3.12/site-packages/superset/extensions/metastore_cache.py
-echo '=== superset_config.py ==='
-cat /var/lib/aecp/superset/superset_config.py
