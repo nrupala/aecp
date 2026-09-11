3db7cec47bfad27a9c615d288cfc62fc624a7989
nrupala <nrupalakolkar@gmail.com>
2026-09-11 15:32:25 -0600

    chore: remove scratch

--- deploy/provision/dbg_rec5.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo journalctl -u aecp-ozone-recon.service --since '-9 min' --no-pager | grep -avE '^--|systemd\[1\]' | head -14
-echo '--- recon socket ---'
-sudo ss -tlnp | grep -E ':(9890|9891)'
--- deploy/provision/dbg_rec6.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-systemctl is-active aecp-ozone-recon
-PID=$(systemctl show -p MainPID --value aecp-ozone-recon)
-echo "mainpid=$PID"
-ps -o pid,pcpu,stat,cmd -p "$PID" | tail -2
-sudo journalctl -u aecp-ozone-recon.service -n 14 --no-pager | grep -avE '^--' | tail -10
