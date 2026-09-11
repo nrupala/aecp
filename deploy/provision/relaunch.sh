4f06bc6066044fc3774c579c4c6fd6fa004ee1b9
nrupala <nrupalakolkar@gmail.com>
2026-09-11 13:20:34 -0600

    chore: remove scratch

--- deploy/provision/relaunch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/aecp && git pull --ff-only -q
-# kill any orphan ozone JVMs holding ports
-sudo pkill -f 'hdds.scm' 2>/dev/null
-sudo pkill -f 'ozone' 2>/dev/null
-sleep 3
-sudo systemctl stop 'aecp-*' 2>/dev/null
-sudo bash deploy/provision/bootstrap.sh 2>&1 | tail -14
