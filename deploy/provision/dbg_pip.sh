506c87042c8f037164c1420b9888ea4ee3001911
nrupala <nrupalakolkar@gmail.com>
2026-09-11 11:58:56 -0600

    chore: remove scratch debug scripts

--- deploy/provision/dbg_pip.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/opt/aecp/venv/bin/pip install "apache-superset==5.0.0" 2>&1 | tail -15
--- deploy/provision/dbg_sa.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-grep -iE '^Requires-Dist: (sqlalchemy|SQLAlchemy)' /opt/aecp/venv/lib/python3.12/site-packages/apache_superset-*.dist-info/METADATA
-/opt/aecp/venv/bin/pip list 2>/dev/null | grep -iE '^SQLAlchemy|Flask-Caching'
-echo '--- pip check summary (first conflicts) ---'
-/opt/aecp/venv/bin/pip check 2>/dev/null | head -6
